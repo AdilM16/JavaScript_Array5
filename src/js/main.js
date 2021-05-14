cf4885058021636817f364ff0b1d27cfadbc3b21
M16 <Adilmohamedmohamed97@gmail.com>
2021-05-14 10:33:19 +0100

    Array

---
+++ src/js/main.js
@@ -0,0 +1,45 @@
+//Exo 1
+let ex ="j'ai faim"
+let tabExo = ["Exo", ex, 3, 6, false];
+console.log(tabExo);
+
+//Exo 2
+console.log(tabExo.length);
+
+//Exo 3
+tabExo.pop();
+console.log(tabExo);
+
+//Exo 4
+tabExo.shift();
+console.log(tabExo);
+
+//Exo 5
+tabExo.push("End", "End2");
+console.log(tabExo);
+
+//Exo 6
+tabExo.unshift()
+
+//Exo 7
+
+
+//Exo 8
+let boite = [];
+boite.push("crab");
+boite.push("aquarium");
+alert(boite);
+boite.unshift("confiture");
+alert(boite);
+boite.push("crevette", "feuille", "baguettes");
+alert(boite);
+boite.shift();
+boite.shift();
+boite.shift();
+boite.unshift("pantalon");
+alert(boite.length);
+boite.pop();
+boite.pop();
+boite.shift();
+boite.shift();
+alert(boite);
